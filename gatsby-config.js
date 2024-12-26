cc4976e73bfd10e592bc6e09ce4e0ecab9c2f95e
mrtosun <mr.tosun@gmail.com>
2024-12-26 12:30:38 +0300

    first push

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,26 +4,38 @@
 module.exports = {
   siteMetadata: {
     title: `mert-wrks25`,
-    siteUrl: `https://www.yourdomain.tld`
+    description: `Here's what I'm working on right now.`,
+    author: `Mert Tosun`,
+    siteUrl: `https://mert.works`
   },
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    // "gatsby-plugin-google-gtag", // Commented out for now
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     }
-  }, "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  ]
 };
---
+++ src/pages/home.md
@@ -0,0 +1,14 @@
+---
+title: "Welcome to My Site"
+date: "2024-01-01"
+---
+
+# Hello, I’m [Your Name]!
+
+This is my personal website. I'll expand it in the future with:
+
+- Projects (possibly embedded from Behance)
+- Micro-blog or Blog references (from LinkedIn or BlueSky)
+- More personal or professional details
+
+For now, thanks for visiting this single-page preview!
